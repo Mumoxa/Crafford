4f1b63d199e9203b40367130c3598348b4a0c094
Mumoxa <christo@mumoxa.co.za>
2026-08-21 09:58:07 +0200

    Add Google Drive contribution placeholder

---
+++ js/google-drive-contributions.js
@@ -0,0 +1,5 @@
+/* Outomaties gegenereer uit die familie se Google Drive. */
+window.CRAFFORD_DRIVE = { memories: [] };
+if (window.CRAFFORD && Array.isArray(window.CRAFFORD.memories)) {
+  window.CRAFFORD.memories.push(...window.CRAFFORD_DRIVE.memories);
+}
